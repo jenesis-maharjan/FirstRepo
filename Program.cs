129efebb2caa61263b0dee5f3bdf1bdffc04f6b0
Jenesis Maharjan <jenesismaharjan@gmail.com>
2025-03-21 14:09:02 +0545

    function implementation of all functions

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using dotnetlab1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Calculate cal = new Calculate();
+        ////cal.calculate();
+        //cal.checkPrime();
+
+        //MultiplicationTable multiply = new MultiplicationTable();
+        //multiply.multiply(5);
+
+        MultiplicationTable mul = new MultiplicationTable();
+        mul.mul();
+
+        //MultiplicationTable hi = new MultiplicationTable();
+        //hi.boxMul();
+    }
+}
