8c9d61322548b8233606bac55aac40d5ca27fe31
Jenesis Maharjan <jenesismaharjan@gmail.com>
2025-03-21 14:36:04 +0545

    C# basic

---
+++ Basics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetlab1
+{
+    class Basics
+    {
+        public void PrintUsername()
+        {
+            Console.WriteLine("enter your name.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your name is : " + name);
+        }
+    }
+}
