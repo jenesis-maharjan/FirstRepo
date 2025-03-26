ee8a961145397df1ee49af3d8f4a9581a98808f8
Jenesis Maharjan <jenesismaharjan@gmail.com>
2025-03-26 07:30:32 +0545

    Dictionary/associative array in c#

---
+++ Dictionarydemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetlab1
+{
+    class Dictionarydemo
+    {
+        Dictionary<string, string> studentDict = new Dictionary<string, string>();
+
+        public void addStudents()
+        {
+            studentDict["name"] = "Aman";
+            studentDict["course"] = "BCA";
+            studentDict.Add("sem", "5");
+            studentDict.Add("age", "21");
+
+            foreach(string val in studentDict.Values)
+            {
+                Console.WriteLine(val);
+            }
+            Console.WriteLine();
+            foreach(string key in studentDict.Keys)
+            {
+                Console.WriteLine(key + ":" + studentDict[key]);
+            }
+            Console.WriteLine();
+            foreach(var student in studentDict)
+            {
+                Console.WriteLine(student.Key + ":" + student.Value);
+            }
+        }
+    }
+}
